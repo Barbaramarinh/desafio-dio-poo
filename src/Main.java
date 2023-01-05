d2da4adf37683034faef8bc3e3539029f673e262
bamar <bamarinho88@hotmail.com>
2023-01-05 10:36:28 -0300

    Desafio-DIO-Poo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import br.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = null;
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = null;
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descricao curso js");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
     }
 }
